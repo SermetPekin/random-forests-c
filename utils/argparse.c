02ce6c528c00f8293370d5bb511e3d83e8ee8521
Sermet Pekin <96650846+SermetPekin@users.noreply.github.com>
2025-09-16 12:11:35 +0300

    Implement argument parsing in argparse.c

---
+++ utils/argparse.c
@@ -0,0 +1,29 @@
+#include "argparse.h"
+
+ 
+
+ 
+ void parse_args(int argc, char **argv, struct arguments *arguments) {
+    // Defaults
+    arguments->rows = 0;
+    arguments->cols = 0;
+    arguments->log_level = 1;
+    arguments->random_seed = 0;
+    arguments->args[0] = NULL;
+
+    for (int i = 1; i < argc; ++i) {
+        if (strcmp(argv[i], ARG_KEY_ROWS) == 0 && i + 1 < argc) {
+            arguments->rows = atol(argv[++i]);
+        } else if (strcmp(argv[i], ARG_KEY_COLS) == 0 && i + 1 < argc) {
+            arguments->cols = atol(argv[++i]);
+        } else if (strcmp(argv[i], ARG_KEY_LOG_LEVEL) == 0 && i + 1 < argc) {
+            arguments->log_level = atoi(argv[++i]);
+        } else if (strcmp(argv[i], ARG_KEY_SEED) == 0 && i + 1 < argc) {
+            arguments->random_seed = atoi(argv[++i]);
+        } else if (arguments->args[0] == NULL) {
+            arguments->args[0] = argv[i]; // CSV file
+        }
+    }
+}
+
+ 
